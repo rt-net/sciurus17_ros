8bcac9df957ef143cceb4ca0c9018bd9107bbbe8
anhquan18 <s17c1054bz@s.chibakoudai.jp>
2020-03-26 17:38:49 +0900

    modify type of arm's target_pose from 'list' to 'geometry_msgs/Pose, Point'

--- sciurus17_examples/scripts/visual_feedback_demo.py
+++ sciurus17_examples/scripts/visual_feedback_demo.py
@@ -19,6 +19,7 @@ import tf
 import sys
 import math
 import os
+import copy
 
 
 class VisualFeedbacker(object):
@@ -61,9 +62,9 @@ class VisualFeedbacker(object):
     def debug_hand_position(self, target_pose):
         if self._check_available_marker():
             ar_relative_pos = self._get_ar_marker_transform()
-            pos_diff_x =  target_pose[0] - (self._marker_pos[0] + ar_relative_pos[0])
-            pos_diff_y =  target_pose[1] - (self._marker_pos[1] + ar_relative_pos[1])
-            pos_diff_z =  target_pose[2] - (self._marker_pos[2] + ar_relative_pos[2])
+            pos_diff_x =  target_pose.position.x - (self._marker_pos[0] + ar_relative_pos[0])
+            pos_diff_y =  target_pose.position.y - (self._marker_pos[1] + ar_relative_pos[1])
+            pos_diff_z =  target_pose.position.z - (self._marker_pos[2] + ar_relative_pos[2])
             
             return [pos_diff_x, pos_diff_y, pos_diff_z]
 
@@ -80,24 +81,47 @@ class VisualFeedbacker(object):
             rospy.logerr(str(e))
             self._terminate()
 
-    def calculate_feedback_pos(self, current_pos, hand_diff):
-        for i, val in enumerate(hand_diff): 
-            if abs(val) <= 0.0004: hand_diff[i] = 0.0 # 0.4mm以下の誤差は位置を補正しない
+    def calculate_feedback_pos(self, target_position, hand_diff):
+        MIN_TRHESHOLD = 0.0004 # meter
+        MAX_TRHESHOLD_Z = 0.0008 # meter
+        feedback_gain = [0.6, 0.6, 2.0, 0.01]
+        feedback_position = copy.deepcopy(target_position)
 
-        pos_x = current_pos[0] + hand_diff[0] *0.6
-        pos_y = current_pos[1] + hand_diff[1] *0.6
-        pos_z = current_pos[2] + hand_diff[2] *(2.0 if abs(hand_diff[2]) >=0.0005 else 0.01)
+        # 0.4mm以下の誤差は位置を補正しない
+        for i in range(3): 
+            if abs(hand_diff[i]) <= MIN_TRHESHOLD: 
+                feedback_gain[i] = 0.0
+
+        feedback_position.x += hand_diff * feedback_gain[0]
+        feedback_position.y += hand_diff * feedback_gain[1]
+        
+        # z軸方向で0.8mm以下の誤差は位置の発散を防ぐためにより小さいゲインを使用する
+        if (feedback_gain[2] != 0.0) and (MIN_TRHESHOLD < abs(hand_diff[2]) < MAX_TRHESHOLD_Z):
+            feedback_position.z += hand_diff * feedback_gain[3]
+        else:
+            feedback_position.z += hand_diff * feedback_gain[2]
 
         self._marker_pos = ()
 
-        return pos_x, pos_y, pos_z
+        return feedback_position
 
     def register_ar_marker(self):
         rospy.loginfo("INITIALIZE ARMS POSITION FOR AR MARKER REGISTRATION")
 
         # ARマーカーが見えるようにカメラとハンドを動かすと
-        np.set_neck_angle(yaw= math.radians(-40.0), pitch= math.radians(-50.0))
-        ac.set_arm_pose(pos= (0.3, -0.1, 0.3), ori= (3.14/2.0, 0.0, 0.0))
+        neck_pitch.set_neck_angle(yaw= math.radians(-40.0), pitch= math.radians(-50.0))
+
+        target_pose = geometry_msgs.msg.Pose() # ハンドの現実の目標位置
+        target_pose.position.x = 0.3
+        target_pose.position.y = -0.1
+        target_pose.position.z = 0.3
+        q = quaternion_from_euler(3.14/2.0, 0.0, 0.0)
+        target_pose.orientation.x = q[0]
+        target_pose.orientation.y = q[1]
+        target_pose.orientation.z = q[2]
+        target_pose.orientation.w = q[3]
+
+        arm_controller.set_arm_pose(target_pose)
 
         # アームの先端のARマーカーを登録 
         self._start_registering_time = rospy.get_rostime().secs
@@ -109,36 +133,21 @@ class VisualFeedbacker(object):
             rospy.logerr("ERROR: THERE IS NO AR MARKER")
             self._terminate()
 
-        ac.initialize_arm_pose()
+        arm_controller.initialize_arm_pose()
         rospy.loginfo("FINISHED REGISTERING AR MARKER ID FOR RIGHT HAND")
 
     def _terminate(self):
         ## SRDFに定義されている"home"の姿勢にする
-        ac.initialize_arm_pose()
+        arm_controller.initialize_arm_pose()
         # 終了
         rospy.signal_shutdown('STOPPING')
         sys.exit(1)
 
 
 class ArmController(object):
-    def set_arm_pose(self, pos=(0.0, 0.0, 0.0), 
-                           ori=(0.0, 0.0, 0.0), 
-                           sleep_time=1.5):
-
-        arm_target_pose = geometry_msgs.msg.Pose()
-        arm_target_pose.position.x = pos[0]
-        arm_target_pose.position.y = pos[1]
-        arm_target_pose.position.z = pos[2]
-        
-        q = quaternion_from_euler(ori[0], ori[1], ori[2])
-        arm_target_pose.orientation.x = q[0]
-        arm_target_pose.orientation.y = q[1]
-        arm_target_pose.orientation.z = q[2]
-        arm_target_pose.orientation.w = q[3]
-
-        
-        rospy.loginfo("Move hand to position:" + str(pos))
-        arm.set_pose_target(arm_target_pose)  # 目標ポーズ設定
+    def set_arm_pose(self, target_pose, sleep_time=1.5):
+        rospy.loginfo("Move hand to position:" + str(target_pose))
+        arm.set_pose_target(target_pose)  # 目標ポーズ設定
         arm.go()  # 実行
         rospy.sleep(sleep_time)
 
@@ -181,59 +190,62 @@ class NeckPitch(object):
 def main():
     # SRDFに定義されている"home"の姿勢にする
     os.system("clear")
-    ac.initialize_arm_pose()
+    arm_controller.initialize_arm_pose()
     rospy.sleep(1.0)
 
     # ARマーカーを登録し始める
-    vf.register_ar_marker()
+    visual_feedbacker.register_ar_marker()
 
     # デモを行う
-    np.set_neck_angle(yaw= math.radians(-10.0), pitch= math.radians(-65.0))
-    target_pos = (0.3, 0.0, 0.161+0.091) # z軸の値 = 三角形の高さ + 六角レンチの先端とハンドの先端間の距離
-    target_ori = (3.14/2.0, 0.0, 0.0)
+    neck_pitch.set_neck_angle(yaw= math.radians(-10.0), pitch= math.radians(-65.0))
 
+    reality_target_pose = geometry_msgs.msg.Pose() # ハンドの現実の目標位置
+    reality_target_pose.position.x = 0.3
+    reality_target_pose.position.y = 0.0
+    reality_target_pose.position.z = 0.161+0.091 # z軸の値 = 三角形の高さ + 六角レンチの先端とハンドの先端間の距離
+    q = quaternion_from_euler(3.14/2.0, 0.0, 0.0)
+    reality_target_pose.orientation.x = q[0]
+    reality_target_pose.orientation.y = q[1]
+    reality_target_pose.orientation.z = q[2]
+    reality_target_pose.orientation.w = q[3]
+
+    moveit_target_pose = copy.deepcopy(reality_target_pose)
 
     # ARマーカーを使わないデモ
-    ac.set_arm_pose(target_pos, target_ori, 1.5)
+    arm_controller.set_arm_pose(reality_target_pose, 1.5)
 
-    diff_before_feedback = vf.debug_hand_position(target_pos)
-    ac.initialize_arm_pose()
+    diff_before_feedback = visual_feedbacker.debug_hand_position(reality_target_pose)
+    arm_controller.initialize_arm_pose()
 
     # ARマーカーでアームの先端位置を5回補正するデモ
-    ac.set_arm_pose(target_pos, target_ori, 1.5)
-    moveit_goal_pos = target_pos
+    arm_controller.set_arm_pose(reality_target_pose, 1.5)
 
     for i in range(5):
         # ARマーカーを使って位置の誤差を計算する
-        hand_diff = vf.debug_hand_position(target_pos)
-        fixed_pose = vf.calculate_feedback_pos(moveit_goal_pos, hand_diff)
+        hand_diff = visual_feedbacker.debug_hand_position(reality_target_pose)
+        feedbacked_position = visual_feedbacker.calculate_feedback_pos(moveit_target_pose.position, hand_diff)
 
+        moveit_target_pose.position = copy.deepcopy(feedbacked_position)
         # 位置を補正し始める
         rospy.loginfo("START FIXING HAND POSITION WITH AR MARKER")
-        ac.set_arm_pose(fixed_pose, target_ori, 1.0)
-        moveit_goal_pos = fixed_pose
+        arm_controller.set_arm_pose(moveit_target_pose, 1.0)
 
-    diff_after_feedback = vf.debug_hand_position(target_pos)
+    diff_after_feedback = visual_feedbacker.debug_hand_position(reality_target_pose)
     
     print('\n')
     print("Position different before:", diff_before_feedback)
     print("Position different after:", diff_after_feedback)
 
-    for i in range(5):
-        rospy.sleep(1.0)
-    ac.initialize_arm_pose()
+    arm_controller.initialize_arm_pose()
 
 
 
 if __name__ == "__main__":
-    '''
-    デモを行う場所
-    '''
     rospy.init_node("sciurus17_visual_feedback")
 
-    np = NeckPitch()
-    ac = ArmController()
-    vf = VisualFeedbacker()
+    neck_pitch = NeckPitch()
+    arm_controller = ArmController()
+    visual_feedbacker = VisualFeedbacker()
 
     # 右腕グループの制御
     arm = moveit_commander.MoveGroupCommander("r_arm_group")
@@ -250,9 +262,4 @@ if __name__ == "__main__":
     gripper_goal = GripperCommandGoal()
     gripper_goal.command.max_effort = 2.0
 
-    r = rospy.Rate(20)
-
-    rospy.sleep(1.0)
-    
     main()
-
