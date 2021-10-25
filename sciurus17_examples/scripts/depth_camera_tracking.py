653a2cc6a15e663c43281e42667462d5b9fab72a
ShotaAk <s.aoki@rt-net.jp>
2021-10-25 18:44:21 +0900

    cameraサンプルをPython3に対応

--- sciurus17_examples/scripts/depth_camera_tracking.py
+++ sciurus17_examples/scripts/depth_camera_tracking.py
@@ -48,7 +48,7 @@ class ObjectTracker:
     def _image_callback(self, ros_image):
         try:
             input_image = self._bridge.imgmsg_to_cv2(ros_image, "bgr8")
-        except CvBridgeError, e:
+        except CvBridgeError as e:
             rospy.logerr(e)
             return
 
@@ -58,7 +58,7 @@ class ObjectTracker:
     def _depth_callback(self, ros_image):
         try:
             input_image = self._bridge.imgmsg_to_cv2(ros_image, "passthrough")
-        except CvBridgeError, e:
+        except CvBridgeError as e:
             rospy.logerr(e)
             return
             
