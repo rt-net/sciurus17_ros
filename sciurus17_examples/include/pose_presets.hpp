e0021d38d493d1ea311f058aca7e74c363ecc0ec
Kuwagata <kuwagata@rt-net.jp>
2023-11-07 20:45:43 +0900

    ポーズプリセットの追加

--- sciurus17_examples/include/pose_presets.hpp
+++ sciurus17_examples/include/pose_presets.hpp
@@ -25,7 +25,10 @@ namespace pose_presets
 class PosePresets
 {
 public:
-  geometry_msgs::msg::Pose downward(double x, double y, double z);
+  geometry_msgs::msg::Pose generate_pose(
+    double x, double y, double z, double roll, double pitch, double yaw);
+  geometry_msgs::msg::Pose right_downward(double x, double y, double z);
+  geometry_msgs::msg::Pose left_downward(double x, double y, double z);
 };
 }  // namespace pose_presets
 
--- sciurus17_examples/src/pose_presets.cpp
+++ sciurus17_examples/src/pose_presets.cpp
@@ -20,15 +20,32 @@
 
 namespace pose_presets
 {
-geometry_msgs::msg::Pose PosePresets::downward(double x, double y, double z)
+geometry_msgs::msg::Pose PosePresets::generate_pose(
+  double x, double y, double z, double roll, double pitch, double yaw)
 {
   geometry_msgs::msg::Pose target_pose;
   tf2::Quaternion q;
   target_pose.position.x = x;
   target_pose.position.y = y;
   target_pose.position.z = z;
-  q.setRPY(angles::from_degrees(90), angles::from_degrees(0), angles::from_degrees(0));
+  q.setRPY(roll, pitch, yaw);
   target_pose.orientation = tf2::toMsg(q);
+  return target_pose;
+}
+
+geometry_msgs::msg::Pose PosePresets::right_downward(double x, double y, double z)
+{
+  geometry_msgs::msg::Pose target_pose;
+  target_pose = generate_pose(x, y, z,
+    angles::from_degrees(90), angles::from_degrees(0), angles::from_degrees(0));
+  return target_pose;
+}
+
+geometry_msgs::msg::Pose PosePresets::left_downward(double x, double y, double z)
+{
+  geometry_msgs::msg::Pose target_pose;
+  target_pose = generate_pose(x, y, z,
+    angles::from_degrees(-90), angles::from_degrees(0), angles::from_degrees(0));
   return target_pose;
 }
 }  // namespace pose_presets
