9b4be0f9242556ecc5e8d421e83c7ac9a6280308
Kuwagata <kuwagata@rt-net.jp>
2023-11-07 20:46:48 +0900

    pick_and_place_left_armサンプル追加

---
+++ sciurus17_examples/src/pick_and_place_left_arm.cpp
@@ -0,0 +1,114 @@
+// Copyright 2023 RT Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Reference:
+// https://github.com/ros-planning/moveit2_tutorials/blob
+// /a547cf49ff7d1fe16a93dfe020c6027bcb035b51/doc/move_group_interface
+// /src/move_group_interface_tutorial.cpp
+
+#include <cmath>
+
+#include "angles/angles.h"
+#include "geometry_msgs/msg/pose.hpp"
+#include "geometry_msgs/msg/quaternion.hpp"
+#include "moveit/move_group_interface/move_group_interface.h"
+#include "rclcpp/rclcpp.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "pose_presets.hpp"
+
+using MoveGroupInterface = moveit::planning_interface::MoveGroupInterface;
+
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("pick_and_place_left_arm");
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::NodeOptions node_options;
+  node_options.automatically_declare_parameters_from_overrides(true);
+  auto move_group_arm_node = rclcpp::Node::make_shared("move_group_arm_node", node_options);
+  auto move_group_gripper_node = rclcpp::Node::make_shared("move_group_gripper_node", node_options);
+  // For current state monitor
+  rclcpp::executors::SingleThreadedExecutor executor;
+  executor.add_node(move_group_arm_node);
+  executor.add_node(move_group_gripper_node);
+  std::thread([&executor]() {executor.spin();}).detach();
+
+  MoveGroupInterface move_group_arm(move_group_arm_node, "l_arm_group");
+  move_group_arm.setMaxVelocityScalingFactor(1.0);  // Set 0.0 ~ 1.0
+  move_group_arm.setMaxAccelerationScalingFactor(1.0);  // Set 0.0 ~ 1.0
+
+  MoveGroupInterface move_group_gripper(move_group_gripper_node, "l_gripper_group");
+  move_group_gripper.setMaxVelocityScalingFactor(1.0);  // Set 0.0 ~ 1.0
+  move_group_gripper.setMaxAccelerationScalingFactor(1.0);  // Set 0.0 ~ 1.0
+  auto gripper_joint_values = move_group_gripper.getCurrentJointValues();
+  double GRIPPER_DEFAULT = 0.0;
+  double GRIPPER_OPEN = angles::from_degrees(-40.0);
+  double GRIPPER_CLOSE = angles::from_degrees(-20.0);
+
+  // SRDFに定義されている"l_arm_init_pose"の姿勢にする
+  move_group_arm.setNamedTarget("l_arm_init_pose");
+  move_group_arm.move();
+
+  // 何かを掴んでいた時のためにハンドを開く
+  gripper_joint_values[0] = GRIPPER_OPEN;
+  move_group_gripper.setJointValueTarget(gripper_joint_values);
+  move_group_gripper.move();
+
+  // 掴む準備をする
+  pose_presets::PosePresets target_pose;
+  move_group_arm.setPoseTarget(target_pose.left_downward(0.25, 0.0, 0.3));
+  move_group_arm.move();
+
+  // 掴みに行く
+  move_group_arm.setPoseTarget(target_pose.left_downward(0.25, 0.0, 0.1));
+  move_group_arm.move();
+
+  // ハンドを閉じる
+  gripper_joint_values[0] = GRIPPER_CLOSE;
+  move_group_gripper.setJointValueTarget(gripper_joint_values);
+  move_group_gripper.move();
+
+  // 持ち上げる
+  move_group_arm.setPoseTarget(target_pose.left_downward(0.25, 0.0, 0.3));
+  move_group_arm.move();
+
+  // 移動する
+  move_group_arm.setPoseTarget(target_pose.left_downward(0.4, 0.0, 0.3));
+  move_group_arm.move();
+
+  // 下ろす
+  move_group_arm.setPoseTarget(target_pose.left_downward(0.4, 0.0, 0.1));
+  move_group_arm.move();
+
+  // ハンドを開く
+  gripper_joint_values[0] = GRIPPER_OPEN;
+  move_group_gripper.setJointValueTarget(gripper_joint_values);
+  move_group_gripper.move();
+
+  // 少しだけハンドを持ち上げる
+  move_group_arm.setPoseTarget(target_pose.left_downward(0.4, 0.0, 0.2));
+  move_group_arm.move();
+
+  // SRDFに定義されている"l_arm_init_pose"の姿勢にする
+  move_group_arm.setNamedTarget("l_arm_init_pose");
+  move_group_arm.move();
+
+  // ハンドを閉じる
+  gripper_joint_values[0] = GRIPPER_DEFAULT;
+  move_group_gripper.setJointValueTarget(gripper_joint_values);
+  move_group_gripper.move();
+
+  rclcpp::shutdown();
+  return 0;
+}
