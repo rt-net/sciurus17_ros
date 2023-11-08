4d33e18246aebc882c3ab188b2a18bb6bb48caf2
Kuwagata <kuwagata@rt-net.jp>
2023-11-08 17:43:02 +0900

    コメント追加

--- sciurus17_examples/include/pose_presets.hpp
+++ sciurus17_examples/include/pose_presets.hpp
@@ -22,9 +22,12 @@
 
 namespace pose_presets
 {
+// Pose型の位置姿勢を作成
 geometry_msgs::msg::Pose generate_pose(
   double x, double y, double z, double roll, double pitch, double yaw);
+// 右グリッパを下に向ける姿勢を作成
 geometry_msgs::msg::Pose right_arm_downward(double x, double y, double z);
+// 左グリッパを下に向ける姿勢を作成
 geometry_msgs::msg::Pose left_arm_downward(double x, double y, double z);
 }  // namespace pose_presets
 
