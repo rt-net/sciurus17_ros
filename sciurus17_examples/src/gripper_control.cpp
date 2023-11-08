d1cf4dc75c30d9a3a96bed22ed48b919eeee0784
Kuwagata <kuwagata@rt-net.jp>
2023-11-08 15:31:17 +0900

    変数名変更

--- sciurus17_examples/src/gripper_control.cpp
+++ sciurus17_examples/src/gripper_control.cpp
@@ -31,22 +31,22 @@ int main(int argc, char ** argv)
   rclcpp::init(argc, argv);
   rclcpp::NodeOptions node_options;
   node_options.automatically_declare_parameters_from_overrides(true);
-  auto move_group_two_arm_node =
-    rclcpp::Node::make_shared("move_group_two_arm_node", node_options);
+  auto move_group_arm_node =
+    rclcpp::Node::make_shared("move_group_arm_node", node_options);
   auto move_group_r_gripper_node =
     rclcpp::Node::make_shared("move_group_r_gripper_node", node_options);
   auto move_group_l_gripper_node =
     rclcpp::Node::make_shared("move_group_l_gripper_node", node_options);
   // For current state monitor
   rclcpp::executors::SingleThreadedExecutor executor;
-  executor.add_node(move_group_two_arm_node);
+  executor.add_node(move_group_arm_node);
   executor.add_node(move_group_r_gripper_node);
   executor.add_node(move_group_l_gripper_node);
   std::thread([&executor]() {executor.spin();}).detach();
 
-  MoveGroupInterface move_group_two_arm(move_group_two_arm_node, "two_arm_group");
-  move_group_two_arm.setMaxVelocityScalingFactor(1.0);  // Set 0.0 ~ 1.0
-  move_group_two_arm.setMaxAccelerationScalingFactor(1.0);  // Set 0.0 ~ 1.0
+  MoveGroupInterface move_group_arm(move_group_arm_node, "two_arm_group");
+  move_group_arm.setMaxVelocityScalingFactor(1.0);  // Set 0.0 ~ 1.0
+  move_group_arm.setMaxAccelerationScalingFactor(1.0);  // Set 0.0 ~ 1.0
 
   MoveGroupInterface move_group_r_gripper(move_group_r_gripper_node, "r_gripper_group");
   move_group_r_gripper.setMaxVelocityScalingFactor(1.0);  // Set 0.0 ~ 1.0
@@ -59,8 +59,8 @@ int main(int argc, char ** argv)
   auto l_gripper_joint_values = move_group_l_gripper.getCurrentJointValues();
 
   // SRDFに定義されている"two_arm_init_pose"の姿勢にする
-  move_group_two_arm.setNamedTarget("two_arm_init_pose");
-  move_group_two_arm.move();
+  move_group_arm.setNamedTarget("two_arm_init_pose");
+  move_group_arm.move();
 
   // 右グリッパ開閉
   r_gripper_joint_values[0] = angles::from_degrees(40);
