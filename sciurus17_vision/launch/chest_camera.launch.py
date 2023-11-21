f542e021006e7d089a81f039ccdb293bd769ea70
Kuwagata <kuwagata@rt-net.jp>
2023-11-21 18:47:34 +0900

    使用していないライブラリのimportを削除

--- sciurus17_vision/launch/chest_camera.launch.py
+++ sciurus17_vision/launch/chest_camera.launch.py
@@ -14,8 +14,6 @@
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
