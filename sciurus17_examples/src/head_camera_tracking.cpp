49f4c90b61b2678601d1a9d3d14967163740a94f
Kuwagata <kuwagata@rt-net.jp>
2023-12-07 19:15:26 +0900

    追従処理をクラス化

--- sciurus17_examples/src/head_camera_tracking.cpp
+++ sciurus17_examples/src/head_camera_tracking.cpp
@@ -22,51 +22,39 @@
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "angles/angles.h"
 
-#include "moveit/move_group_interface/move_group_interface.h"
+#include "std_msgs/msg/float64_multi_array.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "geometry_msgs/msg/point_stamped.hpp"
 #include "sensor_msgs/msg/camera_info.hpp"
 #include "sensor_msgs/msg/image.hpp"
 #include "control_msgs/msg/joint_trajectory_controller_state.hpp"
-#include "tf2/LinearMath/Quaternion.h"
-#include "tf2/LinearMath/Matrix3x3.h"
-#include "tf2_ros/transform_broadcaster.h"
 #include "opencv2/opencv.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
 #include "cv_bridge/cv_bridge.h"
-#include "image_geometry/pinhole_camera_model.h"
 #include "control_msgs/action/follow_joint_trajectory.hpp"
 using std::placeholders::_1;
-using MoveGroupInterface = moveit::planning_interface::MoveGroupInterface;
 using namespace std::chrono_literals;
 
-class ObjectTracker : public rclcpp::Node
+class ColorDetection : public rclcpp::Node
 {
 public:
 
-  ObjectTracker()
-  : Node("head_camera_tracking_2")
+  ColorDetection()
+  : Node("color_detection")
   {
     image_subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
-      "/head_camera/color/image_raw", 10, std::bind(&ObjectTracker::image_callback, this, _1));
+      "/head_camera/color/image_raw", 10, std::bind(&ColorDetection::image_callback, this, _1));
 
     image_thresholded_publisher_ =
       this->create_publisher<sensor_msgs::msg::Image>("image_thresholded", 10);
-  }
 
-  cv::Point2d get_normalized_object_point()
-  {
-    return normalized_object_point_;
-  }
-
-  bool has_object_point()
-  {
-    return has_object_point_;
+    object_point_publisher_ =
+      this->create_publisher<geometry_msgs::msg::PointStamped>("object_detected", 10);
   }
 
 private:
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_subscription_;
   rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_thresholded_publisher_;
+  rclcpp::Publisher<geometry_msgs::msg::PointStamped>::SharedPtr object_point_publisher_;
   rclcpp::TimerBase::SharedPtr timer_;
   cv::Point2d pixel_err_;
   bool has_object_point_ = false;
@@ -142,7 +130,149 @@ private:
         normalized_object_point_.x = translated_object_point.x / msg->width / 2.0;
         normalized_object_point_.y = translated_object_point.y / msg->height / 2.0;
       }
+      geometry_msgs::msg::PointStamped object_point_msg;
+      object_point_msg.header = msg->header;
+      object_point_msg.point.x = normalized_object_point_.x;
+      object_point_msg.point.y = normalized_object_point_.y;
+      object_point_publisher_->publish(object_point_msg);
     }
+  }
+};
+
+class ObjectTracker : public rclcpp::Node
+{
+public:
+
+  ObjectTracker()
+  : Node("object_tracker")
+  {
+    timer_ = this->create_wall_timer(
+      20ms, std::bind(&ObjectTracker::tracking, this));
+
+    state_subscription_ = this->create_subscription<control_msgs::msg::JointTrajectoryControllerState>(
+      "/neck_controller/controller_state", 10, std::bind(&ObjectTracker::state_callback, this, _1));
+
+    object_point_subscription_ = this->create_subscription<geometry_msgs::msg::PointStamped>(
+      "/object_detected", 10, std::bind(&ObjectTracker::point_callback, this, _1));
+
+    angles_publisher_ =
+      this->create_publisher<std_msgs::msg::Float64MultiArray>("/target_angles", 10);
+  }
+
+private:
+  rclcpp::TimerBase::SharedPtr timer_;
+  cv::Point2d pixel_err_;
+  // -1.0 ~ 1.0に正規化された検出位置
+  cv::Point2d normalized_object_point_;
+  rclcpp::Subscription<control_msgs::msg::JointTrajectoryControllerState>::SharedPtr state_subscription_;
+  rclcpp::Subscription<geometry_msgs::msg::PointStamped>::SharedPtr object_point_subscription_;
+  rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr angles_publisher_;
+  double current_yaw_angle_ = 0;
+  double current_pitch_angle_ = 0;
+  std::vector<double> current_angles_;
+  geometry_msgs::msg::PointStamped::SharedPtr object_point_msg_;
+  double yaw_angle_ = 0;
+  double pitch_angle_ = 0;
+  std::vector<double> target_angles_;
+
+  void state_callback(const control_msgs::msg::JointTrajectoryControllerState::SharedPtr msg)
+  {
+    current_angles_ = msg->feedback.positions;
+  }
+
+  void point_callback(const geometry_msgs::msg::PointStamped::SharedPtr msg)
+  {
+    object_point_msg_ = msg;
+  }
+
+  void tracking()
+  {
+    // 追従を開始する物体位置の閾値
+    const double THRESH_X = 0.05;
+    const double THRESH_Y= 0.05;
+
+    // 首角度の初期値
+    const double INITIAL_YAW_ANGLE = 0;
+    const double INITIAL_PITCH_ANGLE = 0;
+
+    // 首可動範囲
+    const double MAX_YAW_ANGLE = angles::from_degrees(120);
+    const double MIN_YAW_ANGLE = angles::from_degrees(-120);
+    const double MAX_PITCH_ANGLE = angles::from_degrees(50);
+    const double MIN_PITCH_ANGLE = angles::from_degrees(-85);
+
+    // 首角度初期化時の制御角度
+    const double RESET_ANGLE_VEL = angles::from_degrees(0.3);
+
+    // 物体が検出されなくなってから初期角度に戻り始めるまでの時間
+    const std::chrono::nanoseconds DETECTION_TIMEOUT = 3s;
+
+    // 首角度制御量
+    // 値が大きいほど追従速度が速くなる
+    const double OPERATION_GAIN_X = 0.05;
+    const double OPERATION_GAIN_Y = 0.05;
+
+    // 追従動作開始フラグ
+    bool look_object = false;
+
+    // 現在の首角度を取得
+    if (current_angles_.empty()) {
+      return;
+    }
+    if (target_angles_.empty()) {
+      target_angles_ = current_angles_;
+      yaw_angle_ = current_angles_[0];
+      pitch_angle_ = current_angles_[1];
+    }
+
+    // 現在時刻
+    auto now = this->get_clock()->now().nanoseconds();
+
+    if (object_point_msg_) {
+      // 物体を検出したとき追従動作開始フラグをtrueにする
+      const auto detected_time = rclcpp::Time(object_point_msg_->header.stamp).nanoseconds();
+      const auto POINT_ELAPSED_TIME = now - detected_time;
+      look_object = POINT_ELAPSED_TIME < DETECTION_TIMEOUT.count();
+    }
+
+    // 物体が検出されたら追従を行う
+    if (look_object) {
+      // 物体検出位置を取得
+      auto object_position = object_point_msg_->point;
+
+      // 追従動作のための首角度を計算
+      if (std::abs(object_position.x) > THRESH_X) {
+        yaw_angle_ -= object_position.x * OPERATION_GAIN_X;
+      }
+      if (std::abs(object_position.y) > THRESH_Y) {
+        pitch_angle_ -= object_position.y * OPERATION_GAIN_Y;
+      }
+    } else {
+      // ゆっくりと初期角度へ戻る
+      auto diff_yaw_angle = INITIAL_YAW_ANGLE - yaw_angle_;
+      if (std::abs(diff_yaw_angle) > RESET_ANGLE_VEL) {
+        yaw_angle_ += std::copysign(RESET_ANGLE_VEL, diff_yaw_angle);
+      } else {
+        yaw_angle_ = INITIAL_YAW_ANGLE;
+      }
+
+      auto diff_pitch_angle = INITIAL_PITCH_ANGLE - pitch_angle_;
+      if (std::abs(diff_pitch_angle) > RESET_ANGLE_VEL) {
+        pitch_angle_ += std::copysign(RESET_ANGLE_VEL, diff_pitch_angle);
+      } else {
+        pitch_angle_ = INITIAL_PITCH_ANGLE;
+      }
+    }
+
+    // 目標首角度を制限角度内に収める
+    yaw_angle_ = std::clamp(yaw_angle_, MIN_YAW_ANGLE, MAX_YAW_ANGLE);
+    pitch_angle_ = std::clamp(pitch_angle_, MIN_PITCH_ANGLE, MAX_PITCH_ANGLE);
+
+    // 目標角度に首を動かす
+    std_msgs::msg::Float64MultiArray target_angles_msg;
+    target_angles_msg.data.push_back(yaw_angle_);
+    target_angles_msg.data.push_back(pitch_angle_);
+    angles_publisher_->publish(target_angles_msg);
   }
 };
 
@@ -157,13 +287,20 @@ public:
       this,
       "neck_controller/follow_joint_trajectory");
 
-    state_subscription_ = this->create_subscription<control_msgs::msg::JointTrajectoryControllerState>(
-      "/neck_controller/controller_state", 10, std::bind(&NeckControl::state_callback, this, _1));
+    angles_subscription_ = this->create_subscription<std_msgs::msg::Float64MultiArray>(
+      "/target_angles", 10, std::bind(&NeckControl::angles_callback, this, _1));
   }
 
-  void send_goal(double yaw_angle, double pitch_angle, double seconds)
-  {
+private:
+  rclcpp_action::Client<control_msgs::action::FollowJointTrajectory>::SharedPtr client_ptr_;
+  rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr angles_subscription_;
+  control_msgs::msg::JointTrajectoryControllerState::SharedPtr neck_state_;
+
+  void angles_callback(const std_msgs::msg::Float64MultiArray::SharedPtr msg) {
     using namespace std::placeholders;
+    auto yaw_angle = msg->data[0];
+    auto pitch_angle = msg->data[1];
+    const double TIME_FROM_START = 0.001;
 
     if(!this->client_ptr_->wait_for_action_server(5s)) {
       RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
@@ -177,40 +314,10 @@ public:
     trajectory_msgs::msg::JointTrajectoryPoint trajectory_point_msg;
     trajectory_point_msg.positions.push_back(yaw_angle);
     trajectory_point_msg.positions.push_back(pitch_angle);
-    trajectory_point_msg.time_from_start = rclcpp::Duration::from_seconds(seconds);
+    trajectory_point_msg.time_from_start = rclcpp::Duration::from_seconds(TIME_FROM_START);
     goal_msg.trajectory.points.push_back(trajectory_point_msg);
 
     this->client_ptr_->async_send_goal(goal_msg);
-  }
-
-  double get_current_yaw_angle()
-  {
-    return current_yaw_angle_;
-  }
-
-  double get_current_pitch_angle()
-  {
-    return current_pitch_angle_;
-  }
-
-  bool has_state()
-  {
-    return has_state_;
-  }
-
-private:
-  rclcpp_action::Client<control_msgs::action::FollowJointTrajectory>::SharedPtr client_ptr_;
-  rclcpp::Subscription<control_msgs::msg::JointTrajectoryControllerState>::SharedPtr state_subscription_;
-  control_msgs::msg::JointTrajectoryControllerState::SharedPtr neck_state_;
-  double current_yaw_angle_ = 0;
-  double current_pitch_angle_ = 0;
-  bool has_state_ = false;
-
-  void state_callback(const control_msgs::msg::JointTrajectoryControllerState::SharedPtr msg)
-  {
-    current_yaw_angle_ = msg->feedback.positions[0];
-    current_pitch_angle_ = msg->feedback.positions[1];
-    has_state_ = true;
   }
 };
 
@@ -222,105 +329,14 @@ int main(int argc, char * argv[])
   node_options.automatically_declare_parameters_from_overrides(true);
 
   rclcpp::executors::MultiThreadedExecutor exec;
-  auto objeckt_tracker_node = std::make_shared<ObjectTracker>();
+  auto color_detection_node = std::make_shared<ColorDetection>();
   auto neck_control_node = std::make_shared<NeckControl>();
-  exec.add_node(objeckt_tracker_node);
+  auto objeckt_tracker_node = std::make_shared<ObjectTracker>();
+  exec.add_node(color_detection_node);
   exec.add_node(neck_control_node);
-  exec.spin_once();
+  exec.add_node(objeckt_tracker_node);
+  exec.spin();
 
-  // 追従を開始する物体位置の閾値
-  const double THRESH_X = 0.05;
-  const double THRESH_Y= 0.05;
-
-  // 首角度の初期値
-  const double INITIAL_YAW_ANGLE = 0;
-  const double INITIAL_PITCH_ANGLE = 0;
-
-  // 首可動範囲
-  const double MAX_YAW_ANGLE = angles::from_degrees(120);
-  const double MIN_YAW_ANGLE = angles::from_degrees(-120);
-  const double MAX_PITCH_ANGLE = angles::from_degrees(50);
-  const double MIN_PITCH_ANGLE = angles::from_degrees(-85);
-
-  // 首角度初期化時の制御角度
-  const double RESET_ANGLE_VEL = angles::from_degrees(0.3);
-
-  // 物体が検出されなくなってから初期角度に戻り始めるまでの時間
-  const std::chrono::nanoseconds DETECTION_TIMEOUT = 1s;
-
-  // 首角度制御量
-  // 値が大きいほど追従速度が速くなる
-  const double OPERATION_GAIN_X = 0.05;
-  const double OPERATION_GAIN_Y = 0.05;
-
-  // 物体検出時の時刻
-  auto detection_timestamp = objeckt_tracker_node->get_clock()->now().nanoseconds();
-
-  // 追従動作開始フラグ
-  bool look_object = false;
-
-  // 現在の首角度を取得
-  while (!neck_control_node->has_state()) {
-    exec.spin_once();
-  }
-  auto yaw_angle = neck_control_node->get_current_yaw_angle();
-  auto pitch_angle = neck_control_node->get_current_pitch_angle();
-
-  while (rclcpp::ok()) {
-    // 現在時刻
-    auto now = objeckt_tracker_node->get_clock()->now().nanoseconds();
-
-    // 物体を検出したとき追従動作開始フラグをtrueにする
-    if (objeckt_tracker_node->has_object_point()) {
-      detection_timestamp = now;
-      look_object = true;
-    } else {
-      // 物体を検出しなくなってから1秒後に初期角度へ戻る
-      auto lost_time = now - detection_timestamp;
-      if (lost_time > DETECTION_TIMEOUT.count()) {
-        look_object = false;
-      }
-    }
-
-    // 物体が検出されたら追従を行う
-    if (look_object) {
-      // 物体検出位置を取得
-      auto object_point = objeckt_tracker_node->get_normalized_object_point();
-
-      // 追従動作のための首角度を計算
-      if (std::abs(object_point.x) > THRESH_X) {
-        yaw_angle -= object_point.x * OPERATION_GAIN_X;
-      }
-      if (std::abs(object_point.y) > THRESH_Y) {
-        pitch_angle -= object_point.y * OPERATION_GAIN_Y;
-      }
-    } else {
-      // ゆっくりと初期角度へ戻る
-      auto diff_yaw_angle = INITIAL_YAW_ANGLE - yaw_angle;
-      if (std::abs(diff_yaw_angle) > RESET_ANGLE_VEL) {
-        yaw_angle += std::copysign(RESET_ANGLE_VEL, diff_yaw_angle);
-      } else {
-        yaw_angle = INITIAL_YAW_ANGLE;
-      }
-
-      auto diff_pitch_angle = INITIAL_PITCH_ANGLE - pitch_angle;
-      if (std::abs(diff_pitch_angle) > RESET_ANGLE_VEL) {
-        pitch_angle += std::copysign(RESET_ANGLE_VEL, diff_pitch_angle);
-      } else {
-        pitch_angle = INITIAL_PITCH_ANGLE;
-      }
-    }
-
-    // 目標首角度を制限角度内に収める
-    yaw_angle = std::clamp(yaw_angle, MIN_YAW_ANGLE, MAX_YAW_ANGLE);
-    pitch_angle = std::clamp(pitch_angle, MIN_PITCH_ANGLE, MAX_PITCH_ANGLE);
-
-    // 目標角度に首を動かす
-    neck_control_node->send_goal(yaw_angle, pitch_angle, 0.01);
-
-    exec.spin_once();
-    loop_rate.sleep();
-  }
   rclcpp::shutdown();
   return 0;
 }
