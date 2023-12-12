63965e2106a0d228068adabbc84ed7943a9670b3
Kuwagata <kuwagata@rt-net.jp>
2023-12-12 20:02:02 +0900

    スタイル修正

--- sciurus17_examples/launch/object_tracking.launch.py
+++ sciurus17_examples/launch/object_tracking.launch.py
@@ -5,31 +5,31 @@ from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
     container = ComposableNodeContainer(
-            name='my_container',
-            namespace='',
-            package='rclcpp_components',
-            executable='component_container',
-            composable_node_descriptions=[
-                ComposableNode(
-                    package='sciurus17_examples',
-                    plugin='sciurus17_examples::ColorDetection',
-                    name='color_detection',
-                    extra_arguments=[{'use_intra_process_comms': True}]
-                    ),
-                ComposableNode(
-                    package='sciurus17_examples',
-                    plugin='sciurus17_examples::ObjectTracker',
-                    name='object_tracker',
-                    extra_arguments=[{'use_intra_process_comms': True}]
-                    ),
-                ComposableNode(
-                    package='sciurus17_examples',
-                    plugin='sciurus17_examples::NeckJtControl',
-                    name='neck_jt_control',
-                    extra_arguments=[{'use_intra_process_comms': True}]
-                    ),
-            ],
-            output='screen',
+        name='tracking_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='sciurus17_examples',
+                plugin='sciurus17_examples::ColorDetection',
+                name='color_detection',
+                extra_arguments=[{'use_intra_process_comms': True}]
+                ),
+            ComposableNode(
+                package='sciurus17_examples',
+                plugin='sciurus17_examples::ObjectTracker',
+                name='object_tracker',
+                extra_arguments=[{'use_intra_process_comms': True}]
+                ),
+            ComposableNode(
+                package='sciurus17_examples',
+                plugin='sciurus17_examples::NeckJtControl',
+                name='neck_jt_control',
+                extra_arguments=[{'use_intra_process_comms': True}]
+                ),
+        ],
+        output='screen',
     )
 
     return launch.LaunchDescription([container])
