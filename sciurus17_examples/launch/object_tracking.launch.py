5d97674d093bbdde4344628cc6b3569bf879f30b
Kuwagata <kuwagata@rt-net.jp>
2023-12-13 14:47:54 +0900

    use_sim_timeオプション追加

--- sciurus17_examples/launch/object_tracking.launch.py
+++ sciurus17_examples/launch/object_tracking.launch.py
@@ -12,12 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import launch
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import ComposableNodeContainer
+from launch_ros.actions import SetParameter
 from launch_ros.descriptions import ComposableNode
 
 
 def generate_launch_description():
+    declare_use_sim_time = DeclareLaunchArgument(
+        'use_sim_time', default_value='false',
+        description=('Set true when using the gazebo simulator.')
+    )
+
     container = ComposableNodeContainer(
         name='tracking_container',
         namespace='',
@@ -46,4 +54,8 @@ def generate_launch_description():
         output='screen',
     )
 
-    return launch.LaunchDescription([container])
+    return LaunchDescription([
+        declare_use_sim_time,
+        SetParameter(name='use_sim_time', value=LaunchConfiguration('use_sim_time')),
+        container
+    ])
