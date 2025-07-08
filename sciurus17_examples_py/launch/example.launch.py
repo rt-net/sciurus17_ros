4ef666dad37d2efca4ed7d0cd8fa609f3e8c5612
KuraZuzu <kurasawa@rt-net.jp>
2025-07-08 17:29:21 +0900

    group_stateのPythonサンプルを追加

--- sciurus17_examples_py/launch/example.launch.py
+++ sciurus17_examples_py/launch/example.launch.py
@@ -47,7 +47,7 @@ def generate_launch_description():
         'example',
         default_value='gripper_control',
         description=('Set an example executable name: '
-                     '[gripper_control, neck_control, waist_control, '
+                     '[gripper_control, pose_groupstate, neck_control, waist_control, '
                      'pick_and_place_right_arm_waist, pick_and_place_left_arm]'))
 
     declare_use_sim_time = DeclareLaunchArgument(
