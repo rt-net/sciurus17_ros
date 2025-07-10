3f08a75123dda13630c612bac92008f023c5661e
KuraZuzu <kurasawa@rt-net.jp>
2025-07-10 14:27:38 +0900

    Revert "joint_valuesのPythonサンプルを移植（角度未調整）"
    This reverts commit 8692df8591615a59646082d90881cbaddcb7cadd.

--- sciurus17_examples_py/launch/example.launch.py
+++ sciurus17_examples_py/launch/example.launch.py
@@ -48,7 +48,7 @@ def generate_launch_description():
         default_value='gripper_control',
         description=('Set an example executable name: '
                      '[gripper_control, pose_groupstate, neck_control, waist_control, '
-                     'pick_and_place_right_arm_waist, pick_and_place_left_arm, joint_values]'))
+                     'pick_and_place_right_arm_waist, pick_and_place_left_arm]'))
 
     declare_use_sim_time = DeclareLaunchArgument(
         'use_sim_time', default_value='false',
--- sciurus17_examples_py/setup.py
+++ sciurus17_examples_py/setup.py
@@ -32,7 +32,6 @@ setup(
             sciurus17_examples_py.pick_and_place_right_arm_waist:main',
             'pick_and_place_left_arm = sciurus17_examples_py.pick_and_place_left_arm:main',
             'pose_groupstate = sciurus17_examples_py.pose_groupstate:main',
-            'joint_values = sciurus17_examples_py.joint_values:main',
         ],
     },
 )
