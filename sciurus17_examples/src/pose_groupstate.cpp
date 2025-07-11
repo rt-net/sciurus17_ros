3b6c23d962ce598d62bf7678e952fc933db725ed
KuraZuzu <kurasawa@rt-net.jp>
2025-07-11 14:37:36 +0900

    ament_uncrustifyのフォーマットを適用

--- sciurus17_examples/src/pose_groupstate.cpp
+++ sciurus17_examples/src/pose_groupstate.cpp
@@ -33,7 +33,7 @@ int main(int argc, char ** argv)
   // For current state monitor
   rclcpp::executors::SingleThreadedExecutor executor;
   executor.add_node(move_group_arm_node);
-  std::thread([&executor]() { executor.spin(); }).detach();
+  std::thread([&executor]() {executor.spin();}).detach();
 
   MoveGroupInterface move_group_arm(move_group_arm_node, "two_arm_group");
   move_group_arm.setMaxVelocityScalingFactor(1.0);      // Set 0.0 ~ 1.0
