c4ffa784d73f7fc29716589b1291865185c137eb
KuraZuzu <kurasawa@rt-net.jp>
2025-07-16 17:55:16 +0900

    左アームで円運動を行うcppプログラム追加

--- sciurus17_examples/launch/example.launch.py
+++ sciurus17_examples/launch/example.launch.py
@@ -59,7 +59,7 @@ def generate_launch_description():
     declare_example_name = DeclareLaunchArgument(
         'example', default_value='gripper_control',
         description=('Set an example executable name: '
-                     '[gripper_control, neck_control, waist_control,'
+                     '[gripper_control, cartesian_path, neck_control, waist_control,'
                      'pick_and_place_right_arm_waist, pick_and_place_left_arm]')
     )
 
