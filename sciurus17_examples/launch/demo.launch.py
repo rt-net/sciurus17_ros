dc37e164aee506353498592cdeea84ce7f39ff55
KuraZuzu <kurasawa@rt-net.jp>
2026-06-18 18:21:43 +0900

    demo.launch.pyからデバイス設定の引数宣言を削除

--- sciurus17_examples/launch/demo.launch.py
+++ sciurus17_examples/launch/demo.launch.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
@@ -21,18 +19,9 @@ from launch.actions import IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from sciurus17_description.robot_description_loader import RobotDescriptionLoader
 
 
 def generate_launch_description():
-    declare_port_name = DeclareLaunchArgument(
-        'port_name', default_value='/dev/sciurus17spine', description='Set port name.'
-    )
-
-    declare_baudrate = DeclareLaunchArgument(
-        'baudrate', default_value='3000000', description='Set baudrate.'
-    )
-
     declare_use_head_camera = DeclareLaunchArgument(
         'use_head_camera', default_value='true', description='Use head camera.'
     )
@@ -41,22 +30,6 @@ def generate_launch_description():
         'use_chest_camera', default_value='true', description='Use chest camera.'
     )
 
-    config_file_path = os.path.join(
-        get_package_share_directory('sciurus17_control'), 'config', 'manipulator_config.yaml'
-    )
-
-    declare_use_mock_components = DeclareLaunchArgument(
-        'use_mock_components', default_value='false', description='Use mock_components or not.'
-    )
-
-    description_loader = RobotDescriptionLoader()
-    description_loader.port_name = LaunchConfiguration('port_name')
-    description_loader.baudrate = LaunchConfiguration('baudrate')
-    description_loader.timeout_seconds = '1.0'
-    description_loader.manipulator_config_file_path = config_file_path
-    description_loader.use_mock_components = LaunchConfiguration('use_mock_components')
-    description = description_loader.load()
-
     move_group = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [
@@ -64,7 +37,6 @@ def generate_launch_description():
                 '/launch/run_move_group.launch.py',
             ]
         ),
-        launch_arguments={'loaded_description': description}.items(),
     )
 
     control_node = IncludeLaunchDescription(
@@ -74,7 +46,6 @@ def generate_launch_description():
                 '/launch/sciurus17_control.launch.py',
             ]
         ),
-        launch_arguments={'loaded_description': description}.items(),
     )
 
     head_camera_node = IncludeLaunchDescription(
@@ -93,11 +64,8 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
-            declare_port_name,
-            declare_baudrate,
             declare_use_head_camera,
             declare_use_chest_camera,
-            declare_use_mock_components,
             move_group,
             control_node,
             head_camera_node,
