404b1b21dde952ec76dbb380fdcec966f0f4d69c
Kuwamai <kuwagata@rt-net.jp>
2026-07-15 13:27:36 +0900

    fix typo

--- sciurus17_control/launch/sciurus17_control.launch.py
+++ sciurus17_control/launch/sciurus17_control.launch.py
@@ -83,7 +83,7 @@ def generate_launch_description():
     declare_use_isaacsim = DeclareLaunchArgument(
         'use_isaacsim',
         default_value='false',
-        description='Use IsaacSim or not.'
+        description='Use Isaac Sim or not.'
     )
 
     declare_gz_control_config_package = DeclareLaunchArgument(
