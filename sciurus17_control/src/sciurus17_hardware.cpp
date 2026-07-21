13140652c6980c0ee904be438be7200a6c52f4b3
Kuwamai <kuwagata@rt-net.jp>
2026-07-21 10:27:58 +0900

    HardwareComponentInterfaceParams対応 (#187)
    HardwareComponentInterfaceParams対応

--- sciurus17_control/src/sciurus17_hardware.cpp
+++ sciurus17_control/src/sciurus17_hardware.cpp
@@ -49,9 +49,9 @@ Sciurus17Hardware::~Sciurus17Hardware()
 }
 
 CallbackReturn Sciurus17Hardware::on_init(
-  const hardware_interface::HardwareInfo & info)
+  const hardware_interface::HardwareComponentInterfaceParams & params)
 {
-  if (hardware_interface::SystemInterface::on_init(info) != CallbackReturn::SUCCESS) {
+  if (hardware_interface::SystemInterface::on_init(params) != CallbackReturn::SUCCESS) {
     return CallbackReturn::ERROR;
   }
 
